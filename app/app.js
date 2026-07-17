a754a3508f98b69ec3ef9f99989fecd4339bcc74
Madhuri Chennupati <madhurichennupati010@gmail.com>
2026-07-17 20:41:31 +0530

    first commit

---
+++ app/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Docker Security Project");
+});
+
+app.listen(3000, () => {
+    console.log("Application Running...");
+});
